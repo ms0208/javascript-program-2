8d250c977ea3636edca154b619c6746823db0cc5
Manav Sharma <102479672+ms0208@users.noreply.github.com>
2023-07-31 09:51:48 +0530

    Update class.js

--- class.js
+++ class.js
@@ -1,9 +1,11 @@
 class student{
 
-    biodata(){
+    biodata()
+    {
         console.log("My Name is manav . My age is 21.MY class is Btech 1yr");
     }
 }
 let obj1= new student();
+
 obj1.biodata()
 
